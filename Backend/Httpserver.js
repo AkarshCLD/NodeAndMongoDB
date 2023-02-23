e2e06e705680da3ba047d0afd7bb5410eabfa552
Akarshgu <akarshgupta705415@gmail.com>
2023-02-23 14:18:14 +0530

    ok

---
+++ Backend/Httpserver.js
@@ -0,0 +1,20 @@
+const http = require("http");
+
+const app = http.createServer((req, res) => {
+    if (req.url == "/") {
+        res.write("hello the server is running")
+        res.end();
+    }
+    else if (req.method==="POST" && req.url==="/signup") {
+
+        req.on("data", (e) => {
+            res.write(e)
+            res.end()
+            console.log(e.toString())
+        })
+
+    }
+
+})
+
+module.exports = app;
